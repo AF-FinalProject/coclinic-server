cd6407016e802034740267e0e80d08adf93e723e
syaisyah <terraaisyah@gmail.com>
2021-07-28 15:43:07 +0700

    change config.js

--- config/config.js
+++ config/config.js
@@ -1,16 +1,16 @@
-const env= process.env.NODE_ENV;
+const env = process.env.NODE_ENV
 
 if (env !== 'production') {
   require('dotenv').config()
 }
 
 const capsEnv = env.toUpperCase()
-const username = process.env["DB_USERNAME" + capsEnv]
+
+const username = process.env["DB_USERNAME_" + capsEnv]
 const password = process.env["DB_PASSWORD_" + capsEnv]
 const database = process.env["DB_NAME_" + capsEnv]
 const host = process.env["DB_HOST_" + capsEnv]
 const dialect = process.env["DB_DIALECT_" + capsEnv]
-
 
 module.exports = {
   "development": {
