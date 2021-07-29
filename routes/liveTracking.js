f46df8e92033862daebe7e0ade199fbcbb9b1a12
adsap <addesaputra11@gmail.com>
2021-07-29 20:40:55 +0700

    detail live tracking

--- routes/liveTracking.js
+++ routes/liveTracking.js
@@ -3,9 +3,8 @@ const LiveTrackingController = require('../controllers/LiveTrackingController')
 const { authAdmin } = require('../middlewares/auth')
 
 
-// router.post('/', authAdmin, LiveTrackingController.add)
+router.put('/:id', authAdmin, LiveTrackingController.update)
 router.get('/:id', authAdmin, LiveTrackingController.detail)
-
 
 
 module.exports = router
