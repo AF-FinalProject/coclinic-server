312049483b82d89948bd10820cb79cbaec1cd7a5
syaisyah <terraaisyah@gmail.com>
2021-07-30 08:42:43 +0700

    debug authentication routes, try wire client

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,9 @@ const router = require('express').Router()
 const userRoutes = require('./users')
 const orderRoutes = require('./orders')
 const trackingRoutes = require('./liveTracking')
-const { authentication } = require('../middlewares/auth')
 
 
 router.use('/', userRoutes)
-router.use(authentication)
 router.use('/orders', orderRoutes)
 router.use('/tracking', trackingRoutes)
 
--- routes/liveTracking.js
+++ routes/liveTracking.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const LiveTrackingController = require('../controllers/LiveTrackingController')
 const { authAdmin } = require('../middlewares/auth')
+const { authentication } = require('../middlewares/auth')
 
-
+router.use(authentication)
 router.put('/:id', authAdmin, LiveTrackingController.update)
 router.get('/:id', authAdmin, LiveTrackingController.detail)
 // kurang router satu lagi untuk delete live tracking? tapi tergantung
