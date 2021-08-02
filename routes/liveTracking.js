d2c8ac664ff9011ae8f5ac3d3e185652520f2545
VenansiusRendy <lierendysetiawan3232@gmail.com>
2021-08-02 21:04:53 +0800

    Change authentication in update
    Change authentication in liveTracking.js routes in put request from authAdmin to authcustomer

--- routes/liveTracking.js
+++ routes/liveTracking.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const LiveTrackingController = require('../controllers/LiveTrackingController')
-const { authAdmin } = require('../middlewares/auth')
+const { authAdmin, authCustomer } = require('../middlewares/auth')
 const { authentication } = require('../middlewares/auth')
 
 router.use(authentication)
-router.put('/:id', authAdmin, LiveTrackingController.update)
+router.put('/:id', authCustomer, LiveTrackingController.update)
 router.get('/:id', authAdmin, LiveTrackingController.detail)
 
 
